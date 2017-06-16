0918dfd808d59c64835883d579cd21491a6f0b40
Wojciech Lisiecki <wojciech.j.lisiecki@gmail.com>
2017-06-16 20:31:54 +0200

    add second critical section

--- projekt.cpp
+++ projekt.cpp
@@ -6,6 +6,7 @@
 #include <time.h>
 #include <vector>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
@@ -14,15 +15,27 @@ using namespace std;
 #define N_SHEETS 4
 #define N_HOUSES 5
 
-#define REQUEST 100
-#define CONFIRMATION 101
-#define EXIT 102
+#define NEED_DEVICES 101
+#define NEED_DEVICES_CONFIRMATION 102
+#define DEVICES_EXIT 103
+#define NEED_HOUSE 104
+#define NEED_HOUSE_CONFIRMATION 105
+#define HOUSES_EXIT 106
+#define NEED_RETURN_DEVICES 107
+#define NEED_RETURN_DEVICES_CONFIRMATION 108
+
+#define HOUSES 201
+#define DEVICES 202
 
 // #define SHOW_LOGS
 
 enum states {
     FREE,
-    WAITING_FOR_RESPONSE
+    WAITING_FOR_DEVICES,
+    WAITING_FOR_HOUSE,
+    HAVE_DEVICES,
+    LEFT_HOUSE,
+    WAITING_RETURN_DEVICES
 };
 
 struct house {
@@ -39,7 +52,7 @@ struct message {
     int freeRecorders;
     int freeFogMachines;
     int freeSheets;
-    house housesQueue[N_HOUSES];
+    house houses[N_HOUSES];
 
     bool operator<(const message& msg) const
     {
@@ -64,9 +77,11 @@ bool ownFogMachine = false;
 bool ownSheet = false;
 int ownedHouseNumber = -1;
 
-int ownTime = 0;
-vector <message> queue;
-house housesQueue[N_HOUSES];
+int devicesTime = 0;
+int housesTime = 0;
+vector <message> housesQueue;
+vector <message> devicesQueue;
+house houses[N_HOUSES];
 MPI_Status status;
 
 // BEGIN initializations
@@ -77,89 +92,167 @@ void initHouses() {
         newHouse.owner = -1;
         newHouse.timeout = 0;
         newHouse.timestamp = MPI_Wtime();
-        housesQueue[i] = newHouse;
+        houses[i] = newHouse;
     }
 }
 // END initializations
 
 // BEGIN handle different response types
-void handleRequest(message response) {
-    queue.push_back(response);
-    sort(queue.begin(), queue.end());
+void handleDevicesRequest(message response) {
+    devicesQueue.push_back(response);
+    sort(devicesQueue.begin(), devicesQueue.end());
     message request;
-    request.time = ownTime;
-    request.type = CONFIRMATION;
+    request.time = devicesTime;
+    request.type = NEED_DEVICES_CONFIRMATION;
     request.owner = tid;
     if (response.owner != tid) {
 
         #ifdef SHOW_LOGS
-        cout << tid << " is sending CONFIRMATION response to processes " << response.owner << "\n";
+        cout << tid << " is sending NEED_DEVICES_CONFIRMATION response to processes " << response.owner << "\n";
         #endif
 
-        MPI_Send(&request, sizeof(message), MPI_BYTE, response.owner, CONFIRMATION, MPI_COMM_WORLD);
+        MPI_Send(&request, sizeof(message), MPI_BYTE, response.owner, NEED_DEVICES_CONFIRMATION, MPI_COMM_WORLD);
     }
 }
 
-void handleConfirmation(message response) {
-    queue.push_back(response);
-    sort(queue.begin(), queue.end());
+void handleReturnDevicesRequest(message response) {
+    devicesQueue.push_back(response);
+    sort(devicesQueue.begin(), devicesQueue.end());
+    message request;
+    request.time = devicesTime;
+    request.type = NEED_RETURN_DEVICES_CONFIRMATION;
+    request.owner = tid;
+    if (response.owner != tid) {
+
+        #ifdef SHOW_LOGS
+        cout << tid << " is sending NEED_RETURN_DEVICES_CONFIRMATION response to processes " << response.owner << "\n";
+        #endif
+
+        MPI_Send(&request, sizeof(message), MPI_BYTE, response.owner, NEED_RETURN_DEVICES_CONFIRMATION, MPI_COMM_WORLD);
+    }
 }
+
+void handleHouseRequest(message response) {
+    housesQueue.push_back(response);
+    sort(housesQueue.begin(), housesQueue.end());
+    message request;
+    request.time = devicesTime;
+    request.type = NEED_HOUSE_CONFIRMATION;
+    request.owner = tid;
+    if (response.owner != tid) {
+
+        #ifdef SHOW_LOGS
+        cout << tid << " is sending NEED_HOUSE_CONFIRMATION response to processes " << response.owner << "\n";
+        #endif
+
+        MPI_Send(&request, sizeof(message), MPI_BYTE, response.owner, NEED_HOUSE_CONFIRMATION, MPI_COMM_WORLD);
+    }
+}
+
+void handleDevicesConfirmation(message response) {
+    devicesQueue.push_back(response);
+    sort(devicesQueue.begin(), devicesQueue.end());
+}
+
+void handleHouseConfirmation(message response) {
+    housesQueue.push_back(response);
+    sort(housesQueue.begin(), housesQueue.end());
+}
+
+void handleReturnDevicesConfirmation(message response) {
+    devicesQueue.push_back(response);
+    sort(devicesQueue.begin(), devicesQueue.end());
+}
+
 
 void handleExit(message response) {
-    for (int i = 0; i < queue.size(); i++) {
-        if((queue[i].type == REQUEST) && (queue[i].owner == response.owner)){
-            queue.erase(queue.begin() + i);
-            i--;
+    if (response.type == DEVICES_EXIT) {
+        freeRecorders = response.freeRecorders;
+        freeFogMachines = response.freeFogMachines;
+        freeSheets = response.freeSheets;
+        for (int i = 0; i < devicesQueue.size(); i++) {
+            if((devicesQueue[i].type == NEED_DEVICES || devicesQueue[i].type == NEED_RETURN_DEVICES) && (devicesQueue[i].owner == response.owner)){
+                devicesQueue.erase(devicesQueue.begin() + i);
+                i--;
+            }
         }
-    }
-    freeRecorders = response.freeRecorders;
-    freeFogMachines = response.freeFogMachines;
-    freeSheets = response.freeSheets;
-    for (int i = 0; i < N_HOUSES; i++) {
-        housesQueue[i] = response.housesQueue[i];
-        if (housesQueue[i].timeout > 0) {
-            housesQueue[i].timestamp = MPI_Wtime() + housesQueue[i].timeout;
-            housesQueue[i].timeout = 0;
+    } else if (response.type == HOUSES_EXIT) {
+        for (int i = 0; i < N_HOUSES; i++) {
+            houses[i] = response.houses[i];
+            if (houses[i].timeout > 0) {
+                houses[i].timestamp = MPI_Wtime() + houses[i].timeout;
+                houses[i].timeout = 0;
+            }
+        }
+        for (int i = 0; i < housesQueue.size(); i++) {
+            if((housesQueue[i].type == NEED_HOUSE) && (housesQueue[i].owner == response.owner)){
+                housesQueue.erase(housesQueue.begin() + i);
+                i--;
+            }
         }
     }
 }
 // END handle different response types
 
-void sendRequest() {
+void sendRequest(int type) {
     message request;
-    request.time = ownTime;
-    request.type = REQUEST;
+    request.time = (type == NEED_HOUSE ? housesTime : devicesTime);
+    request.type = type;
     request.owner = tid;
 
     #ifdef SHOW_LOGS
-    cout << tid << " is sending request to all other processes\n";
+    cout << tid << " is sending request with type " << type << " to all other processes\n";
     #endif
 
     for (int i=0; i<size; i++) {
-        MPI_Send(&request, sizeof(message), MPI_BYTE, i, REQUEST, MPI_COMM_WORLD);
+        MPI_Send(&request, sizeof(message), MPI_BYTE, i, type, MPI_COMM_WORLD);
     }
-    ownTime++;
-    state = WAITING_FOR_RESPONSE;
+    if (type == NEED_DEVICES) {
+        devicesTime++;
+        state = WAITING_FOR_DEVICES;
+    } else if (type == NEED_RETURN_DEVICES) {
+        devicesTime++;
+        state = WAITING_RETURN_DEVICES;
+    } else if (type == NEED_HOUSE) {
+        housesTime++;
+        state = WAITING_FOR_HOUSE;
+    }
 }
 
 // Receive request and call proper handler
 void receiveRequest() {
     message response;
     MPI_Recv(&response, sizeof(message), MPI_BYTE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-    ownTime = max(response.time, ownTime) + 1;
+    if (response.type == NEED_DEVICES || response.type == NEED_RETURN_DEVICES)
+        devicesTime = max(response.time, devicesTime) + 1;
+    else if (response.type == NEED_HOUSE)
+        housesTime = max(response.time, housesTime) + 1;
 
     #ifdef SHOW_LOGS
     cout << tid << " received request of type " << response.type << " from " << response.owner << " with time " << response.time << "\n";
     #endif
 
     switch (response.type) {
-    case REQUEST:
-        handleRequest(response);
+    case NEED_DEVICES:
+        handleDevicesRequest(response);
         break;
-    case CONFIRMATION:
-        handleConfirmation(response);
+    case NEED_HOUSE:
+        handleHouseRequest(response);
         break;
-    case EXIT:
+    case NEED_DEVICES_CONFIRMATION:
+        handleDevicesConfirmation(response);
+        break;
+    case NEED_HOUSE_CONFIRMATION:
+        handleHouseConfirmation(response);
+        break;
+    case NEED_RETURN_DEVICES:
+        handleReturnDevicesRequest(response);
+        break;
+    case NEED_RETURN_DEVICES_CONFIRMATION:
+        handleReturnDevicesConfirmation(response);
+        break;
+    case DEVICES_EXIT:
+    case HOUSES_EXIT:
         handleExit(response);
         break;
     default:
@@ -168,62 +261,100 @@ void receiveRequest() {
 }
 
 // Exit critical section and inform other processed about devices counts
-void exitCriticalSection() {
+void exitCriticalSection(int type) {
     message request;
-    request.type = EXIT;
     request.owner = tid;
     request.time = -1;
-    request.freeRecorders = freeRecorders;
-    request.freeFogMachines = freeFogMachines;
-    request.freeSheets = freeSheets;
-    for (int i = 0; i < N_HOUSES; i++) request.housesQueue[i] = housesQueue[i];
+    if (type == DEVICES) {
+        request.type = DEVICES_EXIT;
+        request.freeRecorders = freeRecorders;
+        request.freeFogMachines = freeFogMachines;
+        request.freeSheets = freeSheets;
 
-    #ifdef SHOW_LOGS
-    cout << tid << " is sending EXIT request to all proccesses\n";
-    #endif
+        #ifdef SHOW_LOGS
+        cout << tid << " is sending DEVICES_EXIT request to all proccesses\n";
+        #endif
 
-    for (int i = 0; i < size; i++) {
-         MPI_Send(&request, sizeof(message), MPI_BYTE, i, EXIT, MPI_COMM_WORLD);
+        for (int i = 0; i < size; i++) {
+            MPI_Send(&request, sizeof(message), MPI_BYTE, i, DEVICES_EXIT, MPI_COMM_WORLD);
+        }
+    } else if (type == HOUSES) {
+        request.type = HOUSES_EXIT;
+        for (int i = 0; i < N_HOUSES; i++) request.houses[i] = houses[i];
+
+        #ifdef SHOW_LOGS
+        cout << tid << " is sending HOUSES_EXIT request to all proccesses\n";
+        #endif
+
+        for (int i = 0; i < size; i++) {
+            MPI_Send(&request, sizeof(message), MPI_BYTE, i, HOUSES_EXIT, MPI_COMM_WORLD);
+        }
     }
 }
 
 // After entering critical section delete confirmations from all other processes
-void deleteAllConfirmations () {
-    for (int i = 0; i < queue.size(); i++)
-        if (queue[i].type == CONFIRMATION) {
-            queue.erase(queue.begin() + i);
-            i--;
-        }
+void deleteAllConfirmations(int type) {
+    if (type == DEVICES) {
+        for (int i = 0; i < devicesQueue.size(); i++)
+            if (devicesQueue[i].type == NEED_DEVICES_CONFIRMATION || devicesQueue[i].type == NEED_RETURN_DEVICES_CONFIRMATION) {
+                devicesQueue.erase(devicesQueue.begin() + i);
+                i--;
+            }
+    } else if (type == HOUSES) {
+        for (int i = 0; i < housesQueue.size(); i++)
+            if (housesQueue[i].type == NEED_HOUSE_CONFIRMATION) {
+                housesQueue.erase(housesQueue.begin() + i);
+                i--;
+            }
+    }
 }
 
 // Check if process if first in queue
-bool isFirst() {
-    for (int i = 0; i < queue.size(); i++) {
-        if (queue[i].type == REQUEST)
-            if (queue[i].owner == tid) return true; else return false;
+bool isFirst(int type) {
+    if (type == DEVICES) {
+        for (int i = 0; i < devicesQueue.size(); i++) {
+            if (devicesQueue[i].type == NEED_DEVICES || devicesQueue[i].type == NEED_RETURN_DEVICES)
+                if (devicesQueue[i].owner == tid) return true; else return false;
+        }
+    } else if (type == HOUSES) {
+        for (int i = 0; i < housesQueue.size(); i++) {
+            if (housesQueue[i].type == NEED_HOUSE)
+                if (housesQueue[i].owner == tid) return true; else return false;
+        }
     }
 }
 
 // Check if process has request confirmations from all other processes 
-bool hasAllConfirmations() {
-    int myTimestamp = -1;
-    for (int i = 0; i < queue.size(); i++)
-        if (queue[i].owner == tid) {
-            myTimestamp = queue[i].time;
-            break;
-        }
-
+bool hasAllConfirmations(int type) {
     int confirmationCounter = 0;
-    for (int i = 0; i < queue.size(); i++)
-        if (queue[i].type == CONFIRMATION && queue[i].time > myTimestamp)
-            confirmationCounter++;
-    
+    int myTimestamp = -1;
+    if (type == DEVICES) {
+        for (int i = 0; i < devicesQueue.size(); i++)
+            if (devicesQueue[i].owner == tid) {
+                myTimestamp = devicesQueue[i].time;
+                break;
+            }
+
+        for (int i = 0; i < devicesQueue.size(); i++)
+            if ((devicesQueue[i].type == NEED_DEVICES_CONFIRMATION || devicesQueue[i].type == NEED_RETURN_DEVICES_CONFIRMATION) && devicesQueue[i].time > myTimestamp)
+                confirmationCounter++;
+    } else if (type == HOUSES) {
+        for (int i = 0; i < housesQueue.size(); i++)
+            if (housesQueue[i].owner == tid) {
+                myTimestamp = housesQueue[i].time;
+                break;
+            }
+
+        for (int i = 0; i < housesQueue.size(); i++)
+            if (housesQueue[i].type == NEED_HOUSE_CONFIRMATION && housesQueue[i].time > myTimestamp)
+                confirmationCounter++;
+    }
     return (confirmationCounter == (size - 1));
 }
 
 // Check if process can enter critical section
-bool canEnterCriticalSection() {
-    return isFirst() && hasAllConfirmations();
+bool canEnterCriticalSection(int type) {
+    return isFirst(type) && hasAllConfirmations(type);
 }
 
 void bookDevices() {
@@ -264,16 +395,16 @@ bool bookHouse() {
     bool foundHouse = false;
     cout << tid << " ";
     for (int i = 0; i < N_HOUSES; i++) {
-        cout << "[" << housesQueue[i].timestamp << "] ";
+        cout << "[" << houses[i].timestamp << "] ";
     }
     for (int i = 0; i < N_HOUSES; i++) {
         double currTime = MPI_Wtime();
         cout << " currTime: " << currTime;
-        if (housesQueue[i].owner < 0 && currTime > housesQueue[i].timestamp) {
-            housesQueue[i].owner = tid;
+        if (houses[i].owner < 0 && currTime > houses[i].timestamp) {
+            houses[i].owner = tid;
             ownedHouseNumber = i;
             foundHouse = true;
-            cout << tid << ". Booking house number " << housesQueue[i].id << ". Hounting house!\n";
+            cout << tid << ". Booking house number " << houses[i].id << ". Hounting house!\n";
             break;
         }
     }
@@ -282,12 +413,12 @@ bool bookHouse() {
 }
 
 void freeHouse() {
-    int timeout = (rand() % 15) + 10;
+    int timeout = (rand() % 5) + 5;
     for (int i = 0; i < N_HOUSES; i++) {
-        if (housesQueue[i].owner == tid) {
-            cout << tid << " leaving house number " << housesQueue[i].id << "\n";
-            housesQueue[i].owner = -1;
-            housesQueue[i].timeout = timeout;
+        if (houses[i].owner == tid) {
+            cout << tid << " leaving house number " << houses[i].id << "\n";
+            houses[i].owner = -1;
+            houses[i].timeout = timeout;
             ownedHouseNumber = -1;
             break;
         }
@@ -312,27 +443,45 @@ int main(int argc, char **argv)
     while(1) {
         usleep( rand()%500000 );
         if (state == FREE) {
-            sendRequest();
-            state = WAITING_FOR_RESPONSE;
+            sendRequest(NEED_DEVICES);
+            state = WAITING_FOR_DEVICES;
+        } else if (state == HAVE_DEVICES) {
+            sendRequest(NEED_HOUSE);
+            state = WAITING_FOR_HOUSE;
+        } else if (state == LEFT_HOUSE) {
+            sendRequest(NEED_RETURN_DEVICES);
+            state = WAITING_RETURN_DEVICES;
         }
         receiveRequest();
-        if (canEnterCriticalSection()) {
-            cout << tid << " is entering critical section\n";
-
-            if (!ownAllDevices()) {
-                bookDevices();
-            } else if (!hasReservedHouse()) {
-                cout << tid << " has all devices, trying to book house\n";
-                bool success = bookHouse();
-                if (success) {
-                    freeHouse();
-                    returnDevices();
-                }
+        if (state == WAITING_FOR_DEVICES && canEnterCriticalSection(DEVICES)) {
+            cout << tid << " is entering devices critical section\n";
+            bookDevices();
+            if (ownAllDevices()) {
+                cout << tid << " has all devices\n";
+                state = HAVE_DEVICES;
+            } else {
+                state = FREE;
             }
+            deleteAllConfirmations(DEVICES);
+            exitCriticalSection(DEVICES);
+            cout << tid << " is leaving devices critical section\n\n";
+        } else if (state == WAITING_FOR_HOUSE && canEnterCriticalSection(HOUSES)) {
+            cout << tid << " is entering houses critical section\n";
+            bool success = bookHouse();
+            if (success) {
+                freeHouse();
+                state = LEFT_HOUSE;
+            }
+            deleteAllConfirmations(HOUSES);
+            exitCriticalSection(HOUSES);
+            cout << tid << " is leaving houses critical section\n\n";
+        } else if (state == WAITING_RETURN_DEVICES && canEnterCriticalSection(DEVICES)) {
+            cout << tid << " is entering devices critical section\n";
+            returnDevices();
             state = FREE;
-            deleteAllConfirmations();
-            exitCriticalSection();
-            cout << tid << " is leaving critical section\n\n";
+            deleteAllConfirmations(DEVICES);
+            exitCriticalSection(DEVICES);
+            cout << tid << " is leaving devices critical section\n\n";
         }
     }
     MPI_Finalize();
